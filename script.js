354b58c6e54488f291e52fa3f6b0a1b852c4f524
karthikeyaan06 <167192928+karthikeyaan06@users.noreply.github.com>
2024-05-02 18:54:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let theme=1;
+function darkmode()
+{
+
+document.querySelector('body').style.backgroundColor=theme?"rgb(17, 32, 21)":""
+document.querySelector('body').style.color=theme?"rgb(255, 255, 255)":""
+document.querySelector('body').style.backgroundImage=theme?"url()":""
+}
